ee03150b61fd9c73c91f88180e0ad6bbb26489e1
Jennifer Cao <jennifer.cao.ca@gmail.com>
2024-03-13 16:49:56 -0400

    test

---
+++ Question.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Question {
+
+    /* instance variables */
+    private String questionText;
+    private List<String> answers;
+    private int correctAnswerIndex;
+
+
+    public Question(String questionText, List<String> answers) {
+        
+    }
+
+    public boolean isCorrectAnswer(int answerIndex) {
+        return false;
+    }
+
+    public String getQuestionText() {
+        return "bruh";
+    }
+
+    public List<String> getAnswers() {
+        
+    }
+
+
+}
