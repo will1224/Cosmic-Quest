2cc167b77769903efeb481db09f3c42267c58b39
Jennifer Cao <jennifer.cao.ca@gmail.com>
2024-03-19 20:03:10 -0400

    added database

--- Question.java
+++ Question.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import org.json.simple.*;
+
 
 public class Question {
 
@@ -7,19 +9,23 @@ public class Question {
     private List<String> answers;
     private int correctAnswerIndex;
 
-
+    /* makes a question */
     public Question(String questionText, List<String> answers) {
-        
+        this.questionText = questionText;
+        this.answers = answers;
     }
 
+    /* checks validity of answer */
     public boolean isCorrectAnswer(int answerIndex) {
         return false;
     }
 
+    /* parses json file for question */
     public String getQuestionText() {
         return "bruh";
     }
 
+    /* returns answer */
     public List<String> getAnswers() {
         
     }
