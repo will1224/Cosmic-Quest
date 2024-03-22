aed5432d24e9ffe3612c7e885d2816f8b70299c3
Jennifer Cao <jennifer.cao.ca@gmail.com>
2024-03-22 13:27:58 -0400

    moved methods around, gameControl now controls game

--- User.java
+++ User.java
@@ -1,9 +1,14 @@
 public class User {
     private String username;
     private String password;
+    private int level;
 
     public User(String username, String password) {
+        level = 0; // temp hardcoded
+    }
 
+    public int getLevel() {
+        return level;
     }
 
 }
