8c903ed0b4bb8767f05ff0769dc14dcd819695b2
Sophia Tong <stong55@uwo.ca>
2024-03-26 18:10:07 +0000

    loginForm.java edited online with Bitbucket
    fixed path to images

--- loginForm.java
+++ loginForm.java
@@ -24,7 +24,7 @@ public class loginForm implements ActionListener {
         loginPage.setExtendedState(JFrame.MAXIMIZED_BOTH);
         loginPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
-        ImageIcon icon = new ImageIcon("images/middle.png");
+        ImageIcon icon = new ImageIcon("images/loginBGD.png");
         BackgroundPanel backgroundPanel = new BackgroundPanel(icon.getImage());
         backgroundPanel.setLayout(new BorderLayout());
     
