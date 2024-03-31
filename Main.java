0c785ebd740f818a1df6eb66503583b39319609a
Geoffrey Kong <hkong47@uwo.ca>
2024-03-30 21:10:11 -0400

    Deleted non-src files

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Accounts accounts = new Accounts();
-        //Start the UI
-        loginForm loginForm = new loginForm(accounts);
-    }
-}
