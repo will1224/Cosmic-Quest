70217d80f00f0b12f0caed23925cd0194655c5da
Geoffrey Kong <79559478+CYBRXT@users.noreply.github.com>
2024-03-31 21:12:08 -0400

    Fixed Accounts.java and created TestHighScore.java and edited TestAccounts.java

--- src/TestAccounts.java
+++ src/TestAccounts.java
@@ -45,15 +45,15 @@ public class TestAccounts {
     @Test
     public void testRegisterAccount() {
         //Case 1: Registering an account that doesn't exist in the account file.
-        assertEquals(true, accounts.registerAccount("testing", "abc123"));
+        assertEquals(true, accounts.registerAccount("testing99", "abc123"));
         //Case 2: Attempting to register an account that already exists.
-        assertEquals(false, accounts.registerAccount("testing", "def456"));
+        assertEquals(false, accounts.registerAccount("testing99", "def456"));
     }
 
     @Test
     public void testAccountExists() {
         //Case 1: Account exists
-        assertEquals(true, accounts.accountExists("testing"));
+        assertEquals(true, accounts.accountExists("testing99"));
         //Case 2: Account does not exist
         assertEquals(false, accounts.accountExists("test-ing"));
     }
@@ -62,30 +62,30 @@ public class TestAccounts {
     @Test
     public void testCorrectPassword() {
         //Case 1: Password is correct
-        assertEquals(true, accounts.correctPassword("testing", "abc123"));
+        assertEquals(true, accounts.correctPassword("testing99", "abc123"));
         //Case 2: Password is incorrect
-        assertEquals(false, accounts.correctPassword("testing", "def456"));
+        assertEquals(false, accounts.correctPassword("testing99", "def456"));
     }
 
     @Test
     public void testLogin() {
         //Case 1: Test logging into an account that exists.
-        assertEquals(true, accounts.login("testing", "abc123"));
+        assertEquals(true, accounts.login("testing99", "abc123"));
         //Case 2: Test logging into an account that does not exist.
         assertEquals(false, accounts.login("random", "password"));
         //Case 3: Test logging into an account with the wrong username.
         assertEquals(false, accounts.login("test-ing", "abc123"));
         //Case 4: Test logging into an account with the wrong password.
-        assertEquals(false, accounts.login("testing", "abc456"));
+        assertEquals(false, accounts.login("testing99", "abc456"));
     }
 
     @Test
     public void testGetAccountData() {
-        JSONObject aData = accounts.getAccountData("testing");
+        JSONObject aData = accounts.getAccountData("testing99");
         JSONObject fData = accounts.getAccountData("test-ing");
         LevelProgress p = new LevelProgress();
         assertNotEquals(null, aData);
-        assertEquals("testing", aData.get("username"));
+        assertEquals("testing99", aData.get("username"));
         assertEquals("abc123", aData.get("password"));
         assertEquals(p.getProgress().toString(), aData.get("progress").toString());
         assertEquals(null, fData);
@@ -104,16 +104,36 @@ public class TestAccounts {
 
     @Test
     public void testUpdateUserProgress() {
-        LevelProgress p = new LevelProgress((JSONArray) accounts.getAccountData("testing").get("progress"));
+        LevelProgress p = new LevelProgress((JSONArray) accounts.getAccountData("testing99").get("progress"));
         p.setCurrentLevelStatus(0, false);
         p.setCurrentLevelStatus(1, true);
         p.setUnlockedStatus(1, true);
-        accounts.updateUserProgress("testing", p.getProgress());
+        accounts.updateUserProgress("testing99", p.getProgress());
 
-        LevelProgress newP = new LevelProgress((JSONArray) accounts.getAccountData("testing").get("progress"));
+        LevelProgress newP = new LevelProgress((JSONArray) accounts.getAccountData("testing99").get("progress"));
         assertEquals(false, newP.getCurrentLevelStatus(0));
         assertEquals(true, newP.getCurrentLevelStatus(1));
         assertEquals(true, newP.getUnlockedStatus(1));
+    }
+
+    @Test
+    public void testGetPlayerScore() {
+        LevelProgress progress = new LevelProgress();
+        JSONArray backup = progress.getProgress();
+
+        progress.setLevelScore(0, 5);
+        progress.setLevelScore(1, 4);
+        progress.setLevelScore(2, 3);
+        progress.setLevelScore(3, 2);
+        progress.setLevelScore(4, 1);
+
+        accounts.updateUserProgress("testing99", progress.getProgress());
+
+        PlayerScore ps = accounts.getPlayerScore("testing99");
+        assertEquals("testing99", ps.getPlayerName());
+        assertEquals(15, ps.getScore());
+
+        accounts.updateUserProgress("testing99", (new LevelProgress()).getProgress());
     }
 
     @Test
