8ab12ba004f4d8c674770381ebfc07afe9ae3398
Ryanna Luo <rluo57@uwo.ca>
2024-04-01 16:48:11 -0400

    fixed comment formatting

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -72,24 +72,24 @@ public class MainMenu implements ActionListener {
         exitGame = createImageButton("images/exitBTN.png");
 
         /** Add components to the button panel */
-        buttonPanel.add(Box.createVerticalGlue()); // Add space at the top */
+        buttonPanel.add(Box.createVerticalGlue()); /** Add space at the top */
         buttonPanel.add(title);
-        buttonPanel.add(Box.createRigidArea(new Dimension(0, 20))); // Add space between title and buttons
-        buttonPanel.add(Box.createVerticalStrut(30)); // Space underneath Button
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 20))); /** Add space between title and buttons */
+        buttonPanel.add(Box.createVerticalStrut(30)); /** Space underneath Button */
         buttonPanel.add(newGame);
-        buttonPanel.add(Box.createVerticalStrut(20)); // Space underneath
+        buttonPanel.add(Box.createVerticalStrut(20)); /** Space underneath */
         buttonPanel.add(continueGame);
-        buttonPanel.add(Box.createVerticalStrut(20)); // Space underneath
+        buttonPanel.add(Box.createVerticalStrut(20)); /** Space underneath */
         buttonPanel.add(selectLevel);
-        buttonPanel.add(Box.createVerticalStrut(20)); // Space underneath
+        buttonPanel.add(Box.createVerticalStrut(20)); /** Space underneath */
         buttonPanel.add(scores);
-        buttonPanel.add(Box.createVerticalStrut(20)); // Space underneath
+        buttonPanel.add(Box.createVerticalStrut(20)); /** Space underneath */
         buttonPanel.add(options);
-        buttonPanel.add(Box.createVerticalStrut(20)); // Space underneath
+        buttonPanel.add(Box.createVerticalStrut(20)); /** Space underneath */
         buttonPanel.add(exitGame);
-        buttonPanel.add(Box.createVerticalGlue()); // Add space at the bottom
+        buttonPanel.add(Box.createVerticalGlue()); /** Add space at the bottom */
 
-        // Add button panel to the frame
+        /** Add button panel to the frame */
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -139,7 +139,7 @@ public class MainMenu implements ActionListener {
             return;
         }
         if (e.getSource() == selectLevel) {
-            new LevelMenu(accounts); // Open the level menu
+            new LevelMenu(accounts); /** Open the level menu */
         } else if (e.getSource() == exitGame) {
             menu.dispose();
             new LoginForm(accounts);
