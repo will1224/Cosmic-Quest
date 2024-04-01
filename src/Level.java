84dc26d9466b646a644f8bb74e1ce13e618a8c3a
Ryanna Luo <rluo57@uwo.ca>
2024-04-01 16:56:56 -0400

    commenting

--- src/Level.java
+++ src/Level.java
@@ -131,7 +131,7 @@ public class Level {
 
         /**Check if the list contains at least 5 questions*/
         if (questionList.size() < 5) {
-            // If not, handle accordingly. For now, we're just printing a message and returning the shuffled list as is.
+            /** If not, handle accordingly. For now, we're just printing a message and returning the shuffled list as is. */
             System.out.println("Warning: Less than 5 questions available. Returning all available questions.");
             return questionList;
         }
